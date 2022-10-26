9f0dc5ee65d8a1457c91b731715137f82d21feaf
Brahim Carrilho <brahim.carrilho@student.thomasmore.be>
2022-10-26 12:41:37 +0200

    added more application properties files

--- src/main/java/com/example/deploydemo/controllers/HomeController.java
+++ src/main/java/com/example/deploydemo/controllers/HomeController.java
@@ -8,10 +8,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @Value("${specialnumber}")
+    @Value("${specialnumber:111}")
     private int specialNumber;
 
-    @GetMapping("/")
+    @GetMapping({"/", "/home"})
     public String home(Model model) {
         model.addAttribute("specialNumber", specialNumber);
         return "home";
